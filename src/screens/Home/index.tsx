95e066cddfe20bb8f78387a9bc4fa3b2936dce22
Luis Eduardo <lfdasilva9@gmail.com>
2022-10-07 22:57:52 -0300

    create button e component

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,7 +1,13 @@
-import { Text, TextInput, View } from 'react-native';
+import { Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { styles } from './styles';
 
+import { Participant } from '../../components/Participant';
+
 export function Home() {
+    function handleParticipantAdd() {
+        console.log('você clicou no botão!');
+    }
+
     return (
         <View style={styles.container}>
             <Text style={styles.eventName}>
@@ -11,12 +17,22 @@ export function Home() {
                 Terça, 26 de setembro de 2022.
             </Text>
 
-            <TextInput
-                style={styles.input}
-                placeholder= "Nome do participantes"
-                placeholderTextColor="#6b6b6b"
-                
-            />
+            <View style = {styles.form}>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Nome do participantes"
+                    placeholderTextColor="#6b6b6b"
+                />
+
+                <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+                    <Text style={styles.buttonText}>
+                        +
+                    </Text>
+                </TouchableOpacity>
+            </View>
+
+            <Participant />
+            <Participant />
         </View>
     );
 }
