b57a75e9ececaf105fd87fe9628f353311797326
Luis Eduardo <lfdasilva9@gmail.com>
2022-10-10 16:44:56 -0300

    new components list e flatlist

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,18 @@
-import { Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { FlatList, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { styles } from './styles';
 
 import { Participant } from '../../components/Participant';
 
 export function Home() {
+    const participants = ['teste1', 'teste2', 'teste3', 'teste4', 'teste5',
+        'teste6', 'teste7', 'teste8', 'teste9', 'teste10'];
+
     function handleParticipantAdd() {
         console.log('você clicou no botão!');
+    }
+
+    function handleParticipantRemove(name: string) {
+        console.log(`você deletou ${name}!`);
     }
 
     return (
@@ -17,7 +24,7 @@ export function Home() {
                 Terça, 26 de setembro de 2022.
             </Text>
 
-            <View style = {styles.form}>
+            <View style={styles.form}>
                 <TextInput
                     style={styles.input}
                     placeholder="Nome do participantes"
@@ -31,8 +38,22 @@ export function Home() {
                 </TouchableOpacity>
             </View>
 
-            <Participant />
-            <Participant />
+            <FlatList
+                data={participants}
+                keyExtractor={item => item}
+                renderItem={({ item }) => (
+                    <Participant
+                        key={item}
+                        name={item}
+                        onRemove={() => handleParticipantRemove('teste')} />
+                )}
+                showsVerticalScrollIndicator={false}
+                ListEmptyComponent = {() => (
+                    <Text style = {styles.listEmptyText}>
+                        Ninguém chegou no evento ainda? Adicione participantes a sua lista de presenças.
+                    </Text>
+                )}
+            />
         </View>
     );
 }
